537f09251a6dfd7e9c62bd6afa564561d8b58db2
Fernando Jesús Ochoa Rosales <fernandojochoar@gmail.com>
2026-04-08 21:09:09 -0400

    pms updated

--- pms/models/document.py
+++ pms/models/document.py
@@ -1,4 +1,7 @@
-from odoo import fields, models, _
+from odoo import api, fields, models, _
+
+import logging
+_logger = logging.getLogger(__name__)
 
 class Documents(models.Model):
     _inherit = ["documents.document"]
@@ -12,3 +15,22 @@ class Documents(models.Model):
     customer = fields.Many2one("res.partner", string="Customer")
     company = fields.Many2one("res.company", string="Company*") # duplicate string
     invoice_link = fields.Char(string="Invoice Link")
+
+    def _sync_payment_type_to_bill(self):
+        for doc in self:
+            if doc.payment_type and doc.res_model == 'account.move' and doc.res_id:
+                bill = self.env['account.move'].sudo().browse(doc.res_id)
+                if bill.exists() and bill.move_type == 'in_invoice':
+                    bill.write({'payment_type_bills': doc.payment_type})
+
+    def write(self, vals):
+        res = super().write(vals)
+        if 'payment_type' in vals:
+            self._sync_payment_type_to_bill()
+        return res
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        docs = super().create(vals_list)
+        docs.filtered(lambda d: d.payment_type)._sync_payment_type_to_bill()
+        return docs
